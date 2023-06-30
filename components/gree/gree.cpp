b7fbcdcd80b91a3ec0071ef5ef0dc770b4228a9a
GrKoR <dr.g.kornev@gmail.com>
2023-06-30 19:34:19 +0400

    fix: packet receiving process

--- components/gree/gree.cpp
+++ components/gree/gree.cpp
@@ -8,6 +8,9 @@ namespace gree {
 static const char *const TAG = "gree";
 
 // block of byte positions in requests/answers
+// GrKoR: I recommend change this approach (byte positions) to structures like gree_raw_packet_t.
+// You can use pointers (and type casting) for assignment structure to the data buffer.
+// It will be much much easier to work with structures instead of array and byte positions
 static const uint8_t FORCE_UPDATE = 7;
 static const uint8_t MODE = 8;
 static const uint8_t MODE_MASK = 0b11110000;
@@ -34,6 +37,49 @@ void GreeClimate::dump_config() {
 }
 
 void GreeClimate::loop() {
+  gree_raw_packet_t *raw_packet = (gree_raw_packet_t *)this->data_read_;
+
+  // если заголовок пакета еще не прочитан, то просматриваем очередь данных в поиске заголовка
+  while (!receiving_packet_ && this->available() >= sizeof(gree_start_bytes_t))
+  {
+    if (this->peek() == GREE_START_BYTE)
+    {
+      // если первый байт стартовый, то надо проверить ещё и второй
+      // читаем оба
+      this->read_array(this->data_read_, sizeof(raw_packet->header.start_bytes));
+
+      // поднимаем флаг приёма пакета, если второй байт тоже стартовый
+      receiving_packet_ = (raw_packet->header.start_bytes.u8x2[1] == GREE_START_BYTE);
+      
+      // если второй байт неправильный, значит это не заголовок
+      // и значит эти два байта из буфера будут проигнорированы, так как флаг не поднят
+      // их можно не обнулять, так как они всё равно будут перезаписаны в следующий раз
+    }
+    else
+    {
+      // если первый байт в очереди не стартовый, то просто читаем его «в никуда» и забываем
+      this->read();
+    }
+  }
+
+  // если флаг приёма пакета установлен, значит заголовок уже загружен
+  // ждём, пока в очереди не будет столько же байт, как указано в заголовке
+  // и только после этого читаем (чтобы не дёргать быйты по одному)
+  if (receiving_packet_ && this->available() >= raw_packet->header.data_length)
+  {
+    // загружаем в пакет все данные, которые указаны в заголовке
+    this->read_array(raw_packet->data, raw_packet->header.data_length);
+
+    dump_message_("Read array", this->data_read_, raw_packet->header.data_length + sizeof(raw_packet->header.start_bytes));
+    read_state_(this->data_read_, raw_packet->header.data_length + sizeof(raw_packet->header.start_bytes));
+    
+    // не забываем после обработки сбросить флаг и обнулить входящий буфер
+    receiving_packet_ = false;
+    memset(this->data_read_, 0, sizeof(this->data_read_));
+  }
+  
+
+  /*
   if (this->available() >= sizeof(this->data_read_)) {
     this->read_array(this->data_read_, sizeof(this->data_read_));
     dump_message_("Read array", this->data_read_, sizeof(this->data_read_));
@@ -46,6 +92,7 @@ void GreeClimate::loop() {
       return;
     read_state_(this->data_read_, sizeof(this->data_read_));
   }
+  */
 }
 
 /*
--- components/gree/gree.h
+++ components/gree/gree.h
@@ -53,6 +53,26 @@ enum ac_louver_H: uint8_t {
   AC_LOUVERH_SWING_MIDDLE_TO_TOP = 0xB0
 };
 
+#define GREE_START_BYTE 0x7E
+
+union gree_start_bytes_t {
+    uint16_t u16;
+    uint8_t u8x2[2];
+};
+
+struct gree_header_t
+{
+  gree_start_bytes_t start_bytes;
+  uint8_t data_length;
+};
+
+struct gree_raw_packet_t
+{
+  gree_header_t header;
+  uint8_t data[1]; // first data byte
+}
+
+
 /*
 class Constants {
   public:
@@ -88,7 +108,9 @@ class GreeClimate : public climate::Climate, public uart::UARTDevice, public Pol
   // Others set to 0x00
   // data_write_[41] = 12; // unknown but not 0x00. TODO
   uint8_t data_write_[47] = {0x7E, 0x7E, 0x2C, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-  uint8_t data_read_[50];
+  uint8_t data_read_[50] = {0};
+
+  bool receiving_packet_ = false;
   // std::set<climate::ClimateSwingMode> supported_swing_modes_{};
 };
 
